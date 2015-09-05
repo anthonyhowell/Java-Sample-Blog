709bae1d3687c3075b63fc9e5c36bd818e68e296
Anthony Howell <anthonyhowell@gmail.com>
2015-09-05 08:41:08 -0400

    page statistics

--- src/main/java/post/PostPage.java
+++ src/main/java/post/PostPage.java
@@ -3,6 +3,12 @@ package post;
 import javax.persistence.*;
 
 /**
+ * A post is comprised with at least one page of copy. If a multi-page
+ * write up is desired, then the copy can be split up amungst several
+ * pages. As long as the page count is higher than the current page,
+ * there should be a link at the bottom of the current page
+ * (e.g.: `next page`)
+ *
  * Created by anthonyhowell on 8/31/15.
  */
 @Entity
@@ -10,5 +16,7 @@ public class PostPage {
 
     private Long id;
     private String body;
+
+    private PostPageStatistics statistics;
 
 }
---
+++ src/main/java/post/PostPageStatistics.java
@@ -0,0 +1,11 @@
+package post;
+
+/**
+ * Created by anthonyhowell on 9/5/15.
+ */
+public class PostPageStatistics {
+
+    private Long wordCount;
+    private Long viewCount;
+
+}
