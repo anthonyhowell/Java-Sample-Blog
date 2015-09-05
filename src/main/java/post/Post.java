1df61fd4582b1b380adf14d66975769f8a3d8138
Anthony Howell <anthonyhowell@gmail.com>
2015-09-05 08:27:15 -0400

    Better multi-page support

--- src/main/java/post/Post.java
+++ src/main/java/post/Post.java
@@ -17,7 +17,7 @@ public class Post {
     private Long id;
     private Long version;
 
-    private String body;
+    private Set<PostPage> pages;
 
     private Set<Asset> relatedAssets;
 
@@ -26,16 +26,7 @@ public class Post {
     private Set<Category> categories;
     private Set<Tag> tags;
 
-    @Embedded
     private PostStatus status;
-
     private PostStatistics statistics;
-
-
-    // Some blog and news sites prefer to split content of a single post
-    // across multiple pages. Based on this setting, there `could` be
-    // auto-generated meta-content for `page-1`, `page-2`, etc. that
-    // would serve as multiple bodies. I dunno about this yet...
-    private int numberOfPages;
 
 }
---
+++ src/main/java/post/PostPage.java
@@ -0,0 +1,11 @@
+package post;
+
+/**
+ * Created by anthonyhowell on 8/31/15.
+ */
+public class PostPage {
+
+    private int number;
+    private String body;
+
+}
