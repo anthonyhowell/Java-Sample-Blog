5065f87efd27cf80146999f94421cadc6329c047
Anthony Howell <anthonyhowell@gmail.com>
2015-09-05 08:49:29 -0400

    fixed typo's

--- src/main/java/post/PostPage.java
+++ src/main/java/post/PostPage.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 
 /**
  * A post is comprised with at least one page of copy. If a multi-page
- * write up is desired, then the copy can be split up amungst several
+ * write up is desired, then the copy can be split up amongst several
  * pages. As long as the page count is higher than the current page,
  * there should be a link at the bottom of the current page
  * (e.g.: `next page`)
--- src/main/java/post/PostPageStatistics.java
+++ src/main/java/post/PostPageStatistics.java
@@ -1,6 +1,8 @@
 package post;
 
 /**
+ * Statistics or an individual page of copy.
+ *
  * Created by anthonyhowell on 9/5/15.
  */
 public class PostPageStatistics {
